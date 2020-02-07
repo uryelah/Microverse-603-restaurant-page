5ab07153fc60670206e2d1a4b0c088cadc40a5f6
sarah <sarachamorro@gmail.com>
2020-02-07 15:17:02 -0300

    Change to development

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  mode: 'production',
+  mode: 'development',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
