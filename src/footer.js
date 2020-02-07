f405d05ca4b4a20a4da6a292cfa5760b4abfb2bb
sarah <sarachamorro@gmail.com>
2020-02-07 17:38:26 -0300

    clean branch

--- src/footer.js
+++
@@ -1,8 +0,0 @@
-const footer = (() => `<footer class="app-footer">
-<ul>
-  <li>
-    2020 - <i class="fab fa-github-alt"></i><a href="https://github.com/uryelah"> uryelah</a>
-  </li>
-</ul>
-</footer>`)()
-export default footer;
